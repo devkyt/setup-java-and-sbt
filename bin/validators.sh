9959d1b14bfd80649774627fdd55cb63b51993e8
Kirill Tikhanskiy <s8ckerpunch@gmail.com>
2023-10-12 10:24:18 +0300

    swap validators

--- bin/validators.sh
+++ bin/validators.sh
@@ -2,6 +2,16 @@
 
 BoldRed='\033[1;31m'
 
+function is_java_version_valid {
+    local JAVA_VERSION=$1
+    local JAVA_VERSIONS=("21", "20", "19", "17", "11", "8")
+
+    if [[ ! ${JAVA_VERSIONS[@]} =~ $JAVA_VERSION ]]; then
+        echo -e "${BoldRed}Error: unavailable Java version. You shoud use one of these: ${JAVA_VERSIONS[@]}" >&2
+        exit 1
+    fi
+}
+
 function is_arch_valid {
     local JAVA_VERSION=$1
     local ARCH=$2
@@ -14,14 +24,4 @@ function is_arch_valid {
     fi
 }
 
-
-function is_java_version_valid {
-    local JAVA_VERSION=$1
-    local JAVA_VERSIONS=("21", "20", "19", "17", "11", "8")
-
-    if [[ ! ${JAVA_VERSIONS[@]} =~ $JAVA_VERSION ]]; then
-        echo -e "${BoldRed}Error: unavailable Java version. You shoud use one of these: ${JAVA_VERSIONS[@]}" >&2
-        exit 1
-    fi
-}
 
